dfc744cd41b7698303c479ff0586733e5453c425
Ebenezer <70787342+SirEctor@users.noreply.github.com>
2021-08-03 18:32:14 -0400

    actual pages have been added

--- app/wsgi.py
+++ app/wsgi.py
@@ -8,15 +8,15 @@ def home():
 	
 @app.route('/login')
 def login():
-	#return render_template('login.html')
-	return('Login Page Here')
+	return render_template('login.html')
+	#return('Login Page Here')
 @app.route('/register')
 def register():
-	#return render_template('register.html')
-	return('Register Page here')
+	return render_template('register.html')
+	#return('Register Page here')
 @app.route('/analytics')
 def analyze():
-	#return render_template('analytics.html')
-	return('Analytics will be here')
+	return render_template('analytics.html')
+	#return('Analytics will be here')
 if __name__ == '__main__':
     app.run(debug=True)
