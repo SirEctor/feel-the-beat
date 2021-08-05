31228884625571a7d373dae5609ee40cffe5d533
Nicole Souydalay <nicolesouydalay@gmail.com>
2021-08-05 11:29:30 -0500

    Edit login and register pages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+from . import db
+import os
+from app.db import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
 
 @app.route('/')
 def home():
@@ -19,32 +26,58 @@ def register():
 @app.route('/adduser', methods= ['POST', 'GET'])
 def adduser():
     if request.method == 'POST':
-        try:
-            uname = request.form['uname']
-            psw = request.form['psw']
-            
-            with sql.connect("database.db") as c:
-                cur = c.cursor()
-                cur.execute("INSERT INTO users (username, password) VALUES (?,?)", (uname, psw) )
-                c.commit()
-                msg = "You have been succesfully registered!"
-        except:
-            c.rollback()
-            msg = "Error in inserting user"
-            
-        finally:
-            return render_template("result.html", msg=msg)
-            c.close()
-            
+        uname = request.form.get('uname')
+        psw = request.form.get('psw')
+        db = get_db()
+        cur = db.cursor()
+        msg = None
+
+        if not uname:
+            msg = 'Username is required.'
+        elif not psw:
+            msg = 'Password is required.'
+        elif cur.execute(
+            'SELECT id FROM user WHERE username = ?', (uname,)
+        ).fetchone() is not None:
+            msg = f"User {uname} is already registered."
+
+        if msg is None:
+            cur.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (uname, generate_password_hash(psw))
+            )
+            db.commit()
+            msg = f"User {uname} created successfully"
+        
+        return render_template("result.html", msg=msg)
+
+@app.route('/confirmlogin', methods= ['POST', 'GET'])
+def confirmlogin():
+    if request.method == 'POST':
+        uname = request.form.get('uname')
+        psw = request.form.get('psw')
+        db = get_db()
+        cur = db.cursor()
+        msg = ""
+        user = cur.execute(
+            'SELECT * FROM user WHERE username = ?', (uname,)
+        ).fetchone()
+
+        if user is None:
+            msg = 'Incorrect username.'
+        elif not check_password_hash(user['password'], psw):
+            msg = 'Incorrect password.'
+
+        if msg is None:
+            msg = "Login Successful"
+        return render_template("result.html", msg=msg)
+
 @app.route('/list')
 def list():
-   con = sql.connect("database.db")
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from users")
-   
-   rows = cur.fetchall();
+   db = get_db()   
+   cur = db.cursor()
+   cur.execute("SELECT * FROM user")
+   rows = cur.fetchall()
    return render_template("list.html",rows = rows)
 
 
