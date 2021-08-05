e88cdd7850aa4531a0ed44858ed2469fab0ac304
Ebenezer <70787342+SirEctor@users.noreply.github.com>
2021-08-05 10:31:11 -0400

    added getjs endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+import requests
+import json 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return render_template('userauth.html')
+    else:
+        return render_template('index.html')
 	
 @app.route('/login')
 def login():
@@ -58,5 +63,23 @@ def analyze():
 def userauth():
     return render_template('userauth.html')
 
+@app.route('/getjs/<jsvar>')
+def get_jsvar(jsvar):
+    data = {'client_id':os.getenv("CLIENT_ID"), 
+            'client_secret':os.getenv("CLIENT_SECRET"), 
+            'grant_type':'authorization_code',
+            'code':jsvar,
+            'redirect_uri':'http://localhost:5000/'
+            }
+    r = requests.post('https://accounts.spotify.com/api/token',data=data)
+    s = json.loads(r.text)
+
+    access_token = s['access_token']
+    token_type = s['token_type']
+    expires_in = s['expires_in']
+    refresh_token = s['refresh_token']
+    scope = s['scope']
+    
+    return render_template('refreshcode.html', headers=r.headers, access_token=access_token, token_type=token_type, expires_in=expires_in, refresh_token=refresh_token,scope=scope)
 if __name__ == '__main__':
     app.run(debug=True)
