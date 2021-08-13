8115a7815ae80d31e284c3f45bb660d8add522c2
Ebenezer <70787342+SirEctor@users.noreply.github.com>
2021-08-12 20:48:14 -0400

    refresh code added to user class

--- app/table_datatypes.py
+++ app/table_datatypes.py
@@ -13,10 +13,17 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=True, nullable=False)
     authorization_code = db.Column(db.String, default=None)
+    refresh_code = db.Column(db.String, default=None)
     authenticated = db.Column(db.Boolean, default=False)
+    
+    def set_refresh_code(self, refresh_code):
+        self.refresh_code = refresh_code
     
     def set_auth_code(self, authorization_code):
         self.authorization_code = authorization_code
+        
+    def give_refresh_code(self):
+        return self.refresh_code
         
     def give_auth_code(self):
         return self.authorization_code
