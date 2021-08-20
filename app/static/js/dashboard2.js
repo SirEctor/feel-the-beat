9bf880fb3c220a81f269613a23667ef7d6a86192
LindaNayeli104 <linda_nayeli104@hotmail.com>
2021-08-19 21:06:35 -0500

    feat: add mood of the selected day

--- app/static/js/dashboard2.js
+++ app/static/js/dashboard2.js
@@ -1,12 +1,31 @@
-const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element = document.querySelector('.date-picker .dates');
-const mth_element = document.querySelector('.date-picker .dates .month .mth');
-const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
-const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
-const days_element = document.querySelector('.date-picker .dates .days');
+const date_picker_element = document.querySelector(".date-picker");
+const selected_date_element = document.querySelector(
+  ".date-picker .selected-date"
+);
+const dates_element = document.querySelector(".date-picker .dates");
+const mth_element = document.querySelector(".date-picker .dates .month .mth");
+const next_mth_element = document.querySelector(
+  ".date-picker .dates .month .next-mth"
+);
+const prev_mth_element = document.querySelector(
+  ".date-picker .dates .month .prev-mth"
+);
+const days_element = document.querySelector(".date-picker .dates .days");
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 let date = new Date();
 let day = date.getDate();
@@ -18,7 +37,7 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-mth_element.textContent = months[month] + ' ' + year;
+mth_element.textContent = months[month] + " " + year;
 
 selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate;
@@ -26,92 +45,122 @@ selected_date_element.dataset.value = selectedDate;
 populateDates();
 
 // EVENT LISTENERS
-date_picker_element.addEventListener('click', toggleDatePicker);
-next_mth_element.addEventListener('click', goToNextMonth);
-prev_mth_element.addEventListener('click', goToPrevMonth);
+date_picker_element.addEventListener("click", toggleDatePicker);
+next_mth_element.addEventListener("click", goToNextMonth);
+prev_mth_element.addEventListener("click", goToPrevMonth);
 
 // FUNCTIONS
-function toggleDatePicker (e) {
-	if (!checkEventPathForClass(e.path, 'dates')) {
-		dates_element.classList.toggle('active');
-	}
+function toggleDatePicker(e) {
+  if (!checkEventPathForClass(e.path, "dates")) {
+    dates_element.classList.toggle("active");
+  }
 }
 
-function goToNextMonth (e) {
-	month++;
-	if (month > 11) {
-		month = 0;
-		year++;
-	}
-	mth_element.textContent = months[month] + ' ' + year;
-	populateDates();
+function goToNextMonth(e) {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  mth_element.textContent = months[month] + " " + year;
+  populateDates();
 }
 
-function goToPrevMonth (e) {
-	month--;
-	if (month < 0) {
-		month = 11;
-		year--;
-	}
-	mth_element.textContent = months[month] + ' ' + year;
-	populateDates();
+function goToPrevMonth(e) {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  mth_element.textContent = months[month] + " " + year;
+  populateDates();
 }
 
-function populateDates (e) {
-	days_element.innerHTML = '';
-	let amount_days = 31;
+function populateDates(e) {
+  days_element.innerHTML = "";
+  let amount_days = 31;
 
-	if (month == 1) {
-		amount_days = 28;
-	}
+  if (month == 1) {
+    amount_days = 28;
+  }
 
-	for (let i = 0; i < amount_days; i++) {
-		const day_element = document.createElement('div');
-		day_element.classList.add('day');
-		day_element.textContent = i + 1;
+  for (let i = 0; i < amount_days; i++) {
+    const day_element = document.createElement("div");
+    day_element.classList.add("day");
+    day_element.textContent = i + 1;
 
-		if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
-			day_element.classList.add('selected');
-		}
+    if (
+      selectedDay == i + 1 &&
+      selectedYear == year &&
+      selectedMonth == month
+    ) {
+      day_element.classList.add("selected");
+    }
 
-		day_element.addEventListener('click', function () {
-			selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
-			selectedDay = (i + 1);
-			selectedMonth = month;
-			selectedYear = year;
+    day_element.addEventListener("click", function () {
+      selectedDate = new Date(year + "-" + (month + 1) + "-" + (i + 1));
+      selectedDay = i + 1;
+      selectedMonth = month;
+      selectedYear = year;
 
-			selected_date_element.textContent = formatDate(selectedDate);
-			selected_date_element.dataset.value = selectedDate;
+      selected_date_element.textContent = formatDate(selectedDate);
+      selected_date_element.dataset.value = selectedDate;
 
-			populateDates();
-		});
+      populateDates();
+      checkMood(); //Check
+    });
 
-		days_element.appendChild(day_element);
-	}
+    days_element.appendChild(day_element);
+  }
 }
 
 // HELPER FUNCTIONS
-function checkEventPathForClass (path, selector) {
-	for (let i = 0; i < path.length; i++) {
-		if (path[i].classList && path[i].classList.contains(selector)) {
-			return true;
-		}
-	}
-	
-	return false;
+function checkEventPathForClass(path, selector) {
+  for (let i = 0; i < path.length; i++) {
+    if (path[i].classList && path[i].classList.contains(selector)) {
+      return true;
+    }
+  }
+
+  return false;
 }
-function formatDate (d) {
-	let day = d.getDate();
-	if (day < 10) {
-		day = '0' + day;
-	}
+function formatDate(d) {
+  let day = d.getDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
 
-	let month = d.getMonth() + 1;
-	if (month < 10) {
-		month = '0' + month;
-	}
+  let month = d.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
 
-	let year = d.getFullYear();
+  let year = d.getFullYear();
 
-	return day + ' / ' + month + ' / ' + year;
+  return day + " / " + month + " / " + year;
+}
+
+function checkMood() {
+  let data = { user_id: "2", date: "2021-08-19 00:00:00" };
+
+  try {
+    if (r == None) {
+      window.alert("You didn't track your mood that day!");
+    }
+    var r = requests.post(
+      "https://feelthebeat.tech/api/daily-record",
+      (json = json.dumps(data))
+    );
+    print(r.json());
+    var rJSON = r.json();
+    var song = rJSON.get("mood");
+    var artist = rJSON.get("name");
+
+    document.querySelector(".dayMood").innerHTML = rJSON.get("artist");
+    document.querySelector(".songArtist").innerHTML = song + "-" + artist;
+
+    //{'artist': 'artist here', 'mood': 'mood here', 'name': 'song name here'}
+  } catch (error) {
+    console.error(error);
+  }
 }
