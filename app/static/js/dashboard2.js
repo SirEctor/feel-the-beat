f7e48fd496c5fcec1f3284ff5c46e5bc1036d2e3
LindaNayeli104 <linda_nayeli104@hotmail.com>
2021-08-20 01:30:20 -0500

    fix: add quotes

--- app/static/js/dashboard2.js
+++ app/static/js/dashboard2.js
@@ -195,30 +195,25 @@ function checkMood() {
 
         switch (mood) {
           case love:
-            document.querySelector(".moodEmogi").innerHTML = (
-              <i class="far fa-grin-hearts emoji love"></i>
-            );
+            document.querySelector(".moodEmogi").innerHTML =
+              '<i class="far fa-grin-hearts emoji love"></i>';
             break;
           case happy:
-            document.querySelector(".moodEmogi").innerHTML = (
-              <i class="far fa-laugh-beam emoji happy"></i>
-            );
+            document.querySelector(".moodEmogi").innerHTML =
+              '<i class="far fa-laugh-beam emoji happy"></i>';
             break;
           case valor2:
-            document.querySelector(".moodEmogi").innerHTML = (
-              <i class="far fa-meh emoji normal"></i>
-            );
+            document.querySelector(".moodEmogi").innerHTML =
+              '<i class="far fa-meh emoji normal"></i>';
             break;
           case sad:
-            document.querySelector(".moodEmogi").innerHTML = (
-              <i class="far fa-frown emoji sad"></i>
-            );
+            document.querySelector(".moodEmogi").innerHTML =
+              '<i class="far fa-frown emoji sad"></i>';
             break;
 
           case angry:
-            document.querySelector(".moodEmogi").innerHTML = (
-              <i class="far fa-angry emoji angry"></i>
-            );
+            document.querySelector(".moodEmogi").innerHTML =
+              '<i class="far fa-angry emoji angry"></i>';
             break;
           default:
             break;
